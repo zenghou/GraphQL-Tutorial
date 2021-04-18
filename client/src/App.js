842d615f373249c69fabd8e22bb74e5fec9f8731
zenghou <zenghoulim@outlook.com>
2021-04-19 04:25:12 +0800

    set up Apollo client

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import BookList from './components/BookList';
+import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { ApolloProvider } from '@apollo/client/react';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql', // endpoint for graphql
+  cache: new InMemoryCache(),
+})
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client={client}>
+      <div className="App">
+       <h1> Ninja's Reading list</h1>
+       <BookList/>
+      </div>
+    </ApolloProvider>
   );
 }
 
---
+++ client/src/components/BookList.js
@@ -0,0 +1,12 @@
+
+function BookList() {
+  return (
+    <div>
+     <ul id="book-list">
+      <li>Book name</li>
+     </ul>
+    </div>
+  );
+}
+
+export default BookList;
