2ae15f6a3a0cc67a37bf1daa61e5b0304d0d3368
zenghou <zenghoulim@outlook.com>
2021-04-19 02:38:57 +0800

    GraphQLNonNull for required values

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -16,6 +16,7 @@ const {
   GraphQLList,
   GraphQLSchema,
   GraphQLID,
+  GraphQLNonNull,
 } = graphql;
 
 const BookType = new GraphQLObjectType({
@@ -98,8 +99,8 @@ const Mutation = new GraphQLObjectType({
     addAuthor: {
       type: AuthorType,
       args: {
-        name: { type: GraphQLString},
-        age: { type: GraphQLInt},
+        name: { type: new GraphQLNonNull(GraphQLString) },
+        age: { type: new GraphQLNonNull(GraphQLInt) },
       },
       resolve(parent, args) {
         // mongoose model
@@ -114,9 +115,9 @@ const Mutation = new GraphQLObjectType({
     addBook: {
       type: BookType,
       args: {
-        name: { type: GraphQLString },
-        genre: { type: GraphQLString },
-        authorId: { type: GraphQLID },
+        name: { type: new GraphQLNonNull(GraphQLString) },
+        genre: { type: new GraphQLNonNull(GraphQLString) },
+        authorId: { type: new GraphQLNonNull(GraphQLID) },
       },
       resolve(parent, args) {
         let book = new Book({
